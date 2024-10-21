8bc90998c887240dc84da58319aa4464db590bdd
bug-py <cocorigolo6@gmail.com>
2024-10-21 14:08:08 +0200

    détail

--- Static.js
+++ Static.js
@@ -1,5 +1,6 @@
 let titre=document.title
 let icon=document.querySelector("link[rel='icon']")
 let href=icon.getAttribute('href')
+console.log(href)
 window.addEventListener("focus",()=>{document.title=titre;icon.setAttribute("href",href);})
 window.addEventListener("blur",()=>{document.title="Reviens 🫵";icon.setAttribute("href","./img/Alert.png")})
